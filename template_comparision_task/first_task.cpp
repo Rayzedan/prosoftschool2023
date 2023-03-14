c375978e214e0f3c275c597414d8957316136c15
Daniil Bezlepkin <bezlepkin_d01@mail.ru>
2023-03-14 15:04:01 +0500

    add realization for first task

---
+++ template_comparision_task/first_task.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "first_task.h"
+
+int main() 
+{
+	NewA test(1, 4);
+	NewA test1(2, 1);
+	NewB testStr("abc");
+	NewB testStr1("cba");
+
+	//Сравнение между NewA
+	std::cout << (test <= test1) << (test >= test1) << (test == test1) << (test != test1) << '\n';
+
+	//Сравнение с int
+	std::cout << (test <= 1) << (test >= 1) << (test == 1) << (test != 2) << '\n';
+
+	//Сравнение с float
+	std::cout << (test <= 1.0f) << (test >= 1.0f) << (test == 1.0f) << (test != 2.0f) << '\n';
+
+	//Сравнение с double
+	std::cout << (test <= 1.0) << (test >= 1.0) << (test == 1.0) << (test != 2.0) << '\n';
+
+	//Сравнение с char
+	std::cout << (test <= '1') << (test >= '3') << (test == '2') << (test != '3') << '\n';
+
+	//Сравнение строк
+	//std::cout << (testStr <= ) << (testDouble >= 1.0) << (testDouble == 1.0) << (testDouble != 2.0) << '\n';
+
+	std::cout << "All tests passed successfully!" << '\n';
+	return 0;
+}
--- template_comparision_task/first_task.h
+++ template_comparision_task/first_task.h
@@ -1,31 +1,133 @@
-class NewA
-{
+#include <string>
+#include <string_view>
+
+template<typename T>
+class OtherComparisionOperators {
 public:
-    NewA(int a, int b) : m_a(a), m_b(b){}
+	template <typename A>
+	constexpr bool operator==(const A& other) const {
+		return !(Value() > other) && !(Value() < other);
+	}
 
-    // Операторы сравнения с NewA
-    bool operator<(const NewA& other) const
-    {
-        return (m_a < other.m_a) && (m_b < other.m_b);
-    }
+	template <typename A>
+	constexpr bool operator!=(const A& other) const {
+		return (Value() > other) || (Value() < other);
+	}
+	template <typename A>
+	constexpr bool operator<=(const A& other) const {
+		return !(Value() > other);
+	}
 
-    bool operator>(const NewA& other) const
-    {
-        return (m_a > other.m_a) && (m_b > other.m_b);
-    }
+	template <typename A>
+	constexpr bool operator>=(const A& other) const {
+		return !(Value() < other);
+	}
+private:
+	constexpr T const& Value() const
+	{
+		return static_cast<const T&>(*this);
+	}
+	OtherComparisionOperators() = default;
+	~OtherComparisionOperators() = default;
+	OtherComparisionOperators<T>(const OtherComparisionOperators<T>& other) = default;
+	OtherComparisionOperators<T>(OtherComparisionOperators<T>&& other) = default;
+	OtherComparisionOperators<T>& operator=(const OtherComparisionOperators<T>& other) = default;
+	OtherComparisionOperators<T>& operator=(OtherComparisionOperators<T>&& other) = default;
 
-    // Операторы сравнения с int
-    bool operator<(const int other) const
-    {
-        return m_a < other;
-    }
+	friend T;
+};
 
-    bool operator>(const int other) const
-    {
-        return m_a > other;
-    }
+class NewA : public OtherComparisionOperators<NewA> {
+public:
+	NewA(int a, int b) : m_a(a), m_b(b) {}
+
+	// Операторы сравнения с NewA
+	bool operator<(const NewA& other) const
+	{
+		return (m_a < other.m_a) && (m_b < other.m_b);
+	}
+
+	bool operator>(const NewA& other) const
+	{
+		return (m_a > other.m_a) && (m_b > other.m_b);
+	}
+
+	// Операторы сравнения с int
+	bool operator<(const int other) const
+	{
+		return m_a < other;
+	}
+
+	bool operator>(const int other) const
+	{
+		return m_a > other;
+	}
+
+	// Операторы сравнения с double
+	bool operator<(const double other) const
+	{
+		return m_a < static_cast<int>(other);
+	}
+
+	bool operator>(const double other) const
+	{
+		return m_a > static_cast<int>(other);
+	}
+
+	// Операторы сравнения с char
+	bool operator<(const char other) const
+	{
+		return m_a < static_cast<int>(other);
+	}
+
+	bool operator>(const char other) const
+	{
+		return m_a > static_cast<int>(other);
+	}
+
+	// Операторы сравнения с float
+	bool operator<(const float other) const
+	{
+		return m_a < static_cast<int>(other);
+	}
+
+	bool operator>(const float other) const
+	{
+		return m_a > static_cast<int>(other);
+	}
 
 private:
-    int m_a = 0;
-    int m_b = 0;
+	int m_a = 0;
+	int m_b = 0;
+};
+
+class NewB : public OtherComparisionOperators<NewB>
+{
+public:
+	NewB(std::string_view val) : m_stringView(val) {}
+
+	// Операторы сравнения с NewB
+	bool operator<(const NewB& other) const
+	{
+		return m_stringView < other.m_stringView;
+	}
+
+	bool operator>(const NewB& other) const
+	{
+		return m_stringView > other.m_stringView;
+	}
+
+	// Операторы сравнения с std::string
+	bool operator<(const std::string& other) const
+	{
+		return m_stringView < other;
+	}
+
+	bool operator>(const std::string& other) const
+	{
+		return m_stringView > other;
+	}
+
+private:
+	std::string_view m_stringView;
 };
