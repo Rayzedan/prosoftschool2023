c7ab21a318a690d01e59558e34de6175a2cea9cf
Daniil Bezlepkin <bezlepkin_d01@mail.ru>
2023-03-16 11:13:24 +0500

    fix tab

--- template_comparision_task/first_task.cpp
+++ template_comparision_task/first_task.cpp
@@ -5,27 +5,27 @@ int main()
 {
     NewA test(1, 2);
     NewA test1(3, 3);
-	NewB testStr("abc");
-	NewB testStr1("cba");
+    NewB testStr("abc");
+    NewB testStr1("vba");
 
-	//Сравнение между NewA
+    //Сравнение между NewA
     std::cout << (test <= test1) << (test >= test1) << (test == test1) << (test != test1) << '\n';
 
-	//Сравнение с int
+    //Сравнение с int
     std::cout << (test <= 1) << (test >= 1) << (test == 2) << (test != 2) << '\n';
 
-	//Сравнение с float
-	std::cout << (test <= 1.0f) << (test >= 1.0f) << (test == 1.0f) << (test != 2.0f) << '\n';
+    //Сравнение с float
+    std::cout << (test <= 1.0f) << (test >= 1.0f) << (test == 1.0f) << (test != 2.0f) << '\n';
 
-	//Сравнение с double
-	std::cout << (test <= 1.0) << (test >= 1.0) << (test == 1.0) << (test != 2.0) << '\n';
+    //Сравнение с double
+    std::cout << (test <= 1.0) << (test >= 1.0) << (test == 1.0) << (test != 2.0) << '\n';
 
-	//Сравнение с char
-	std::cout << (test <= '1') << (test >= '3') << (test == '2') << (test != '3') << '\n';
+    //Сравнение с char
+    std::cout << (test <= '1') << (test >= '3') << (test == '2') << (test != '3') << '\n';
 
-	//Сравнение между NewB
-	std::cout << (testStr <= testStr1) << (testStr >= testStr1) << (testStr == testStr1) << (testStr != testStr1) << '\n';
+    //Сравнение между NewB
+    std::cout << (testStr <= testStr1) << (testStr >= testStr1) << (testStr == testStr1) << (testStr != testStr1) << '\n';
 
-	std::cout << "All tests passed successfully!" << '\n';
-	return 0;
+    std::cout << "All tests passed successfully!" << '\n';
+    return 0;
 }
